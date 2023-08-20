97a6eda5a52b18bfe65a03a530ca79cca01de2bf
ad-world <67097683+ad-world@users.noreply.github.com>
2023-08-19 20:03:27 -0400

    Redo login page in Chakra UI

--- frontend/src/util/constants.ts
+++ frontend/src/util/constants.ts
@@ -1,1 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const DEV_API_URL = import.meta.env.VITE_DEV_API_URL;
+export const PROD_API_URL = import.meta.env.VITE_PROD_API_URL;
+export const API_URL = PROD_API_URL;
+
+export const BG_GRADIENT = "linear(to-r, purple.200, pink.400)";
